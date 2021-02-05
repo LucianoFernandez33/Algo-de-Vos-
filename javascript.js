fa418c1c6cf1941acdef9735ab147f9a1921c595
Luciano Fernandez <fer.luciano1987@gmail.com>
2021-02-05 10:30:32 -0300

    agregando carrito de compras a la web

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 let carrito = [];
+if (localStorage.getItem("carrito") != null) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    document.getElementById("contador").innerHTML = carrito.length;
+  }
 
-class producto {
+class Producto {
     constructor(nombreProducto, descripProducto, precioProducto, stockProducto, imagenProducto) {
         this.nombre = nombreProducto;
         this.descripcion = descripProducto;
@@ -10,28 +14,28 @@ class producto {
     }
 }
 
-let productoUno = new producto (
+let productoUno = new Producto (
     "Combo para cumples",
     "Este combo es lo más lindo que vas a ver hoy: vasos, platos, sorbetes!",
     "$ - CONSULTAR",
     5,
     "img/card1.jpg",
 )
-let productoDos = new producto (
+let productoDos = new Producto (
     "Cortinas para cumples",
     "Cortinas en pastel degradé!",
     "$ - CONSULTAR",
     3,
     "img/cortinas.jpg",
 )
-let productoTres = new producto (
+let productoTres = new Producto (
     "Banderines",
     "Kit deco fiestas!",
     "$ - CONSULTAR",
     1,
     "img/fiestas.jpg"
 )
-let productoCuatro = new producto (
+let productoCuatro = new Producto (
     "AMONG US",
     "una temática que furor en este tiempo: cake topper banderines + imágenes para decorar pochocleritas ",
     "$ - CONSULTAR",
@@ -39,17 +43,81 @@ let productoCuatro = new producto (
     "img/cumple.jpg",
 )
 
-//let imagen1 = new Image(400, 600);
-//imagen1.src = 'img/cortinas.jpg';
-//document.body.appendChild(imagen1);
+let productoCinco = new Producto (
+    "Velas SPARKLERS",
+    "Súper lindas y delicadas para decorar tus tortas",
+    "$ - CONSULTAR",
+    10,
+    "img/velas.jpg"
+)
+let productoSeis = new Producto (
+    "Fotolibro",
+    "Regalo expecial para el Día de la Madre, Fotolibro de 15x20cm con 20 fotos, frases y espacio para escribirle + tarjeta con 2 chocolates marroc",
+    "$ - CONSULTAR",
+    5,
+    "img/fotolibro.jpeg",
+)
+
+let productoSiete = new Producto (
+    "Pizarras Imantadas",
+    "Kit de pizarras imantadas con fibras para dibujar",
+    "$ - CONSULTAR",
+    3,
+    "img/pizarras.jpeg",
+)
+
+let productoOcho = new Producto (
+    "TikTok",
+    "Las cartas contienen diferentes retos, challenge y desafíos de TikTok",
+    "$ - CONSULTAR",
+    2,
+    "img/tiktok.jpeg",
+)
+let productoNueve = new Producto (
+    "Kit de Cumple",
+    " Kit de cumple con motivos de La Granja de Zenon",
+    "$ - CONULTAR",
+    1,
+    "img/combo-cumple.jpeg",
+)
+let productoDiez = new Producto (
+    "Caketoppers",
+    "Kit de decoración Animalitos del Bosque",
+    "$ - CONSULTAR",
+    1,
+    "img/animales.jpeg",
+)
+
+let productoOnce = new Producto (
+    "Imánes",
+    "Abecedario, vocales, números y signo imántados",
+    "$ - CONSULTAR",
+    2,
+    "img/letras.jpeg",
+)
+let productoDoce = new Producto (
+    "Etiquetas Personalizadas",
+    " Logo más etiquetas",
+    "$ - CONULTAR",
+    4,
+    "img/tarjetas.jpeg",
+)
 
 let baseDeDatos = [
     productoUno,
     productoDos,
     productoTres,
     productoCuatro,
+    productoCinco,
+    productoSeis,
+    productoSiete,
+    productoOcho,
+    productoNueve,
+    productoDiez,
+    productoOnce,
+    productoDoce,  
 ];
-
+// mostrar cards de productos en pagina de inicio index.html
 let aux = ``;
     for (let i = 0; i < baseDeDatos.length; i++){
         console.log(baseDeDatos[i]);
@@ -61,9 +129,12 @@ let aux = ``;
           <div class="card-body">
             <h4 class="card-title">"${baseDeDatos[i].nombre}"</h4>
             <p class="card-text">"${baseDeDatos[i].descripcion}"</p>
+            <p class="card-text">"${baseDeDatos[i].precio}"</p>
           </div>
           <div class="card-footer">
-            <a href="#" class="btn btn-primary">"${baseDeDatos[i].precio}"</a>
+            <button class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify(
+                baseDeDatos[i]
+              )})'>Agregar Al Carrito</button>
           </div>
         </div>
       </div>`;
@@ -74,3 +145,31 @@ let aux = ``;
     }
 
     document.getElementById("productos").innerHTML = aux;
+
+    // Funcionamiento del carrito
+    // Agregando productos al carrito
+
+    function agregarAlCarrito(producto) {
+        carrito.push(producto);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+      
+        let aux = 0;
+        for (let i = 0; i < carrito.length; i++) {
+          aux += carrito[i].precio;
+        }
+        document.getElementById("contador").innerHTML = carrito.length;
+    }
+    //borrar un producto del carrito
+    function borrarUnProducto() {
+        const nuevoCarrito = [];
+        for (let i = 0; i < carrito.length; i++) {
+          if (i != 1) {
+            nuevoCarrito.push(carrito[i]);
+          }
+        }
+        localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+        carrito = nuevoCarrito;
+        document.getElementById("contador").innerHTML = carrito.length;
+      }
+
+      
